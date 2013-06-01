bd8a835c5459aeb3dff18fa99ac7efecce432aa2
Lake Watkins <Nemquae@gmail.com>
2013-06-01 11:35:38 +0800

    otw - Fixed the lack of player collision bug.

--- Assets/OpenCog Assets/Scripts/OpenCog/Physics/Collision/MapCharacterCollision.cs
+++ Assets/OpenCog Assets/Scripts/OpenCog/Physics/Collision/MapCharacterCollision.cs
@@ -14,35 +14,35 @@ public class MapCharacterCollision {
 	}
 	
 	private static Contact? GetClosestContact(OpenCog.Map.OCMap map, CharacterCollider collider) {
-//		Vector3 pos = collider.transform.position;
-//		int x1 = Mathf.FloorToInt(pos.x-collider.radius);
-//		int y1 = Mathf.FloorToInt(pos.y);
-//		int z1 = Mathf.FloorToInt(pos.z-collider.radius);
-//		
-//		int x2 = Mathf.CeilToInt(pos.x+collider.radius);
-//		int y2 = Mathf.CeilToInt(pos.y+collider.height);
-//		int z2 = Mathf.CeilToInt(pos.z+collider.radius);
-//		
-//		Contact? contact = null;
-//		for(int x=x1; x<=x2; x++) {
-//			for(int y=y1; y<=y2; y++) {
-//				for(int z=z1; z<=z2; z++) {
-//					OpenCog.Map.OCBlockData block = map.GetBlock(x, y, z);
-//					if(block.IsSolid()) {
-//						Contact? _newContact = BlockCharacterCollision.GetContactBlockCharacter(new Vector3i(x, y, z), pos, collider);
-//						if(_newContact.HasValue && _newContact.Value.delta.magnitude > float.Epsilon) {
-//							Contact newContact = _newContact.Value;
-//							if(!contact.HasValue || newContact.sqrDistance > contact.Value.sqrDistance) {
-//								contact = newContact;
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//
-//		return contact;
-		return null;
+		Vector3 pos = collider.transform.position;
+		int x1 = Mathf.FloorToInt(pos.x-collider.radius);
+		int y1 = Mathf.FloorToInt(pos.y);
+		int z1 = Mathf.FloorToInt(pos.z-collider.radius);
+		
+		int x2 = Mathf.CeilToInt(pos.x+collider.radius);
+		int y2 = Mathf.CeilToInt(pos.y+collider.height);
+		int z2 = Mathf.CeilToInt(pos.z+collider.radius);
+		
+		Contact? contact = null;
+		for(int x=x1; x<=x2; x++) {
+			for(int y=y1; y<=y2; y++) {
+				for(int z=z1; z<=z2; z++) {
+					OpenCog.Map.OCBlockData block = map.GetBlock(x, y, z);
+					if(block.IsSolid()) {
+						Contact? _newContact = BlockCharacterCollision.GetContactBlockCharacter(new Vector3i(x, y, z), pos, collider);
+						if(_newContact.HasValue && _newContact.Value.delta.magnitude > float.Epsilon) {
+							Contact newContact = _newContact.Value;
+							if(!contact.HasValue || newContact.sqrDistance > contact.Value.sqrDistance) {
+								contact = newContact;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return contact;
+//		return null;
 	}
 	
 }
