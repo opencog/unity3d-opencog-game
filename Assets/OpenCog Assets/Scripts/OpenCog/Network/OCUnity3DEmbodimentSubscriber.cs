37b5e54f21be6ee9e94a398f3cc50cfd16773718
Oliver Timberlake Watkins Junior <Nemquae@gmail.com>
2013-11-25 18:05:22 +0800

    otw - JsonFx and ZeroMQ stubs added.

---
+++ Assets/OpenCog Assets/Scripts/OpenCog/Network/OCUnity3DEmbodimentSubscriber.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class OCUnity3DEmbodimentSubscriber : MonoBehaviour
+{
+
+// Use this for initialization
+void Start()
+{
+	
+}
+	
+// Update is called once per frame
+void Update()
+{
+	
+}
+}
+
